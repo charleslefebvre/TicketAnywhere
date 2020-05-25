233d8e7a95ba0fcd351ee794f2540d62c417afe7
charleslefebvre <58051166+charleslefebvre@users.noreply.github.com>
2020-05-24 21:56:00 -0400

    Revert "Update du cart"

--- LOGIC/modifyTicketNumber.logic.php
+++ LOGIC/modifyTicketNumber.logic.php
@@ -1,16 +1,4 @@
 <?php
-    session_start();
     $numberOfTickets = $_POST['ticketsNewCount'];
-    $showId = $_POST['showId'];
-    $representationId = $_POST['representationId'];
-
-    $cart = $_SESSION['panier'];
-    for($i = 0; $i < count($cart); ++$i){
-        if($cart[$i]['showId'] == $showId && $cart[$i]['representationId'] == $representationId){
-            $cart[$i]['numberOfTickets'] = $numberOfTickets;
-            echo $numberOfTickets;
-            $_SESSION['panier'] = $cart;
-            break;
-        }
-    }
+    echo $numberOfTickets;
 ?>
