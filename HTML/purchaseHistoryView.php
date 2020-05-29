70666f0dce9ff75e2899d821cba3e9f8f486dbc8
Dylann Fontus <58182692+Dylannord@users.noreply.github.com>
2020-05-28 22:23:19 -0400

    Numero de confirmation
    Ajout du numéro de confirmation

--- HTML/purchaseHistoryView.php
+++ HTML/purchaseHistoryView.php
@@ -5,11 +5,19 @@
         header("Location: ../index.php");
         die();
     }
+
+    $longueurKey = 12;
+    $key = "";
+    for($i=1;$i<longueurKey;$i++)
+    {
+        $key .= mt_rand(0,9);
+    }
 ?>
 <p id="title">Purchase History</p>
 <div id="container">
     <div id="list">
-        <?php 
+        <h3>Numéro de confirmation: <?php echo $key ?></h3>
+        <?php
             $purchase = new Purchase();
             $purchase->loadPurchase($_SESSION['userID']);
         ?>
